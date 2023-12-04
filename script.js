7c52465e3985f6aff87b0b75c5a5f192294a7da1
husanjonmusayev <husanjonm279@gmail.com>
2023-12-04 11:53:25 +0500

    uy ishi

---
+++ script.js
@@ -0,0 +1,137 @@
+// 1-masala
+// for(let i=100; i>=1
+//     ; i-- ){
+//     console.log(i);
+// }
+
+// 2 masala
+// let n = +prompt("ihtioriy sonni kiriting")
+// let sum = 0
+// for(let i =1; i<=n; i++){
+// if(i%2==1){
+//     sum+=i
+// }
+// }
+// console.log(sum);
+
+
+// 3 masala
+// let n= +prompt("ihtioriy sonni kriting")
+// let sum = 1
+// for(let i = 1; i<=n; i++){
+//     if(i%3==0){
+//         sum*=i
+//     }
+// }
+// console.log(sum);
+
+// 4 masala
+// let a = +prompt("brinchi sonni kriting")
+// let b = +prompt('ikkinchi sonni kiriting')
+// for(let i =a; i<=b; i++){
+// if( i%3==0){
+//  console.log(i);  
+// }
+// }
+
+//5 masala
+// let a =  +prompt("brinchi sonni kriting")
+// let b =  +prompt('ikkinchi sonni kiriting')
+// let sum = 0;
+// for(let i =a; i<=b; i++){
+// if( i%7==0 && i%5==0){
+//  sum+=i 
+// }
+// }
+// console.log(sum);
+
+// 6 masala
+// let a =  1  //+prompt("birinchi sonni kiriting")
+// let b =  10  //+prompt("ikkinchi sonni kiriting")
+// let res =0
+// for(let i = a; i<=b; i++ ){
+//     if(i%2==1){
+//      res+=i*i   
+//     }
+// }
+// console.log(res);
+
+// 7-masala
+// let n = 12 // +prompt("sonni kiriting")
+// let res 
+// for(let i =1; i<=n; i++){
+
+// if(i%2==0){
+//     res=i
+//     if(n%res==0){
+//         console.log(res);
+//     }
+// }
+// }
+
+// 8 masala
+// let n = 12 // +prompt("sonni kiriting")
+// let res 
+// let caunt =0
+// for(let i =1; i<=n; i++){
+
+// if(i%2==0){
+//     res=i
+//     if(n%res==0){
+//         caunt++
+        
+//     }
+// }
+// }
+// console.log(caunt);
+
+//9 -masala
+// let n =+prompt("sonni kriting")
+// for (let i=2; i<=n; i++){
+// if(i%2==1){
+//     if(n%i==0){
+//         console.log(true);
+//     }
+
+// }
+// }
+
+
+
+// 10 masala
+// let n = +prompt('nomalumni kirgizing')
+// (n**3 % 2 !==0) ? ("qiziqarli") : ("qiziqarli emas")
+
+// 11 masala
+// let n = 180 
+// res =n/5
+// let o = res/5
+// console.log(Math.trunc(o));
+
+// 12 masala 
+// let n = +prompt('sonni kiriting')
+// let kelvin = n + 273.15;
+// let frangeyt = n * 1.80 + 32.00
+// console.log(kelvin);
+// console.log(frangeyt);
+
+//13 masala
+// let n = 10   //+prompt('sonni kiriting')
+// let res = 0
+// for(let i = 1; i<=n; i++){
+//     if(i%3==0){
+//        res+=i
+//     }
+// }
+// console.log(res);
+
+//14 masala
+// let n = +prompt('sonni kiriting')
+// let res =0
+// for(let i =1; i<=n; i++ ){
+//     if(i%3==0 || i%5==0 || i%7==0){
+//         res+=i
+//     }
+// }
+// console.log(res);
+
